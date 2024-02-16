4325dcda339e5b115a06401aecda0829b4f3e91b
dhpyun <dhpyun@dbinc.co.kr>
2024-02-16 17:51:14 +0900

    Initial commit

---
+++ lib/screen/basic_appbar_tabbar_screen.dart
@@ -0,0 +1,14 @@
+import 'package:flutter/material.dart';
+
+class BasicAppBarTabBarScreen extends StatelessWidget {
+  const BasicAppBarTabBarScreen({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: Column(
+        children: [],
+      ),
+    );
+  }
+}
